3cf674cc6ffd08c5040f11cf7fae30a8d007d9cf
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-12-03 16:14:40 +0200

    added AlertTests

--- MyObjects/Helpers/Url.cs
+++ MyObjects/Helpers/Url.cs
@@ -9,5 +9,8 @@ namespace MyObjects.Helpers
     public static class Url
     {
         public const string saucedemoUrl = "https://www.saucedemo.com/";
+        public const string guru99AlertUrl = "http://demo.guru99.com/test/delete_customer.php";
+        public const string guru99PopupUrl = "http://demo.guru99.com/popup.php";
+
     }
 }
